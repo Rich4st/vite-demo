bd33ec3f49fe5d13ea2dde933c7b538947f15f99
Rich4st <50189508+Rich4st@users.noreply.github.com>
2022-05-05 17:02:48 +0800

    feature: 安装NProgress顶部页面加载提示并配置了加载条的样式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
+import NProgress from 'nprogress';
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -6,6 +7,17 @@ const router = createRouter({
   routes: [
     { path: '/', component: () => import('../components/HelloWorld.vue') },
   ],
+});
+
+// 配置路由守卫
+router.beforeEach((to, from) => {
+  if (!NProgress.isStarted()) {
+    NProgress.start();
+  }
+});
+
+router.afterEach((to, from) => {
+  NProgress.done();
 });
 
 export default router;
